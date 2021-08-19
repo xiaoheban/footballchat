b40cf4185d5428407e679bf815367295889b4d13
funeasy <15161451946@163.com>
2021-08-20 06:39:25 +0800

    文件读写

--- src/demo/store/commonFile.go
+++ src/demo/store/commonFile.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
-func testFileAction() {
+func TestFileAction() {
 	//准备数据
 	bytes := []byte("hello world")
 	//ioutil读写
@@ -21,6 +22,23 @@ func testFileAction() {
 	}
 	//os读写
 	//写
-
+	file1, _ := os.Create("osFile.txt") //如果文件已经存在会清空，否则会新建文件
+	defer file1.Close()                 //用完需要关闭文件
+	writtenLen, err := file1.Write(bytes)
+	if err != nil {
+		//发生了错误
+		panic(err)
+	}
 	//读
+	//接收缓冲区
+	readBuffer := make([]byte, writtenLen)
+	file2, _ := os.Open("osFile.txt")
+	defer file2.Close()
+	readLen, err := file2.Read(readBuffer)
+	if err != nil {
+		panic(err)
+	}
+	if readLen > 0 {
+		fmt.Println("read file ", string(readBuffer))
+	}
 }
