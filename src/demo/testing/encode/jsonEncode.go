bd829b325e9fb902d475ff0340fce2d439d3f9ee
funeasy <15161451946@163.com>
2021-09-03 07:11:51 +0800

    isis

--- src/demo/testing/encode/jsonEncode.go
+++ src/demo/testing/encode/jsonEncode.go
@@ -1,8 +1,49 @@
 package encode
 
-func decode(a int) bool {
-	return a > 100
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   string    `json:"author"`
+	Comments []Comment `json:"comments"`
 }
-func encode() bool {
-	return true
+type Comment struct {
+	Id      int    `json:"id"`
+	Author  string `json:"author"`
+	Content string `json:"content"`
+}
+
+func decode(fileName string) (post Post, err error) {
+	var bytes []byte
+	bytes, err = ioutil.ReadFile(fileName)
+	err = json.Unmarshal(bytes, &post)
+	return
+}
+
+/**
+将数据写入json文件
+*/
+func encode(fileName string, post Post) error {
+	datas, err := json.MarshalIndent(&post, "", "\t\t")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(fileName, datas, 0644)
+}
+
+//未实现方法
+func advanceMethod() {
+	//TD DO
+}
+
+//耗时方法
+func methodWithLongTime() {
+	fmt.Println("method test long time")
+	time.Sleep(10 * time.Second)
 }
--- src/demo/testing/encode/jsonEncode_test.go
+++ src/demo/testing/encode/jsonEncode_test.go
@@ -1,14 +1,32 @@
 package encode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestEncode(t *testing.T) {
-	if encode() == false {
-		t.Error()
+	var post Post
+	err := encode("test.json", post)
+	if err != nil {
+		t.Error(err)
 	}
 }
 func TestDecode(t *testing.T) {
-	if decode(1000) == false {
+	post, err := decode("test.json")
+	if err != nil {
 		t.Fail()
 	}
+	fmt.Println(post)
+}
+
+func TestAdvanceMethod(t *testing.T) {
+	//该方法还没有实现是要跳过的
+	t.Skip("not implemented")
+}
+func TestMethodWithLongTime(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skip because long")
+	}
+	methodWithLongTime()
 }
