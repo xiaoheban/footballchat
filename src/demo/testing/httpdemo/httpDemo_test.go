f0fbe83288797892d7a819172e563885e6405902
funeasy <15161451946@163.com>
2021-09-09 07:07:55 +0800

    add user info

--- src/demo/testing/httpdemo/httpDemo_test.go
+++ src/demo/testing/httpdemo/httpDemo_test.go
@@ -1,6 +1,7 @@
 package httpdemo
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -12,12 +13,29 @@ func TestHandleAddUser(t *testing.T) {
 	mux.HandleFunc("/addUser/", handleAddUser)
 	//创建recorder
 	writer := httptest.NewRecorder()
+	//writer.Body = nil
 	userJson := strings.NewReader(`"id":"1","name":"json"`)
 	request, _ := http.NewRequest("POST", "/addUser/", userJson)
 	mux.ServeHTTP(writer, request)
-	//获取请求
+	//处理响应码
+	if writer.Code != 200 {
+		t.Errorf("Response Code is %v", writer.Code)
+	}
 }
 
 func TestHandleGetUser(t *testing.T) {
-
+	mux := http.NewServeMux()
+	mux.HandleFunc("/getUser/", handleGetUser)
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/getUser?userId=1", nil)
+	mux.ServeHTTP(writer, request)
+	if writer.Code != 200 {
+		t.Fatalf("Response Code is %v", writer.Code)
+	}
+	var userInfo User
+	err := json.Unmarshal(writer.Body.Bytes(), &userInfo)
+	if err != nil {
+		t.Fatalf("parser Json Failed %v", err)
+	}
+	t.Logf("the user is %v", userInfo)
 }
