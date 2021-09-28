2065a88688444e715d1ccb889208405a25fb3c46
funeasy <15161451946@163.com>
2021-09-29 07:16:07 +0800

    增加路由文件

---
+++ src/footballChat/index.go
@@ -0,0 +1,10 @@
+/**
+index和公共处理函数
+**/
+package footballchat
+
+import "net/http"
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("这是一个足球迷论坛网站")
+	mux := http.NewServeMux()
+	mux.HandleFunc()
+	server := http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: nil,
+	}
+	server.ListenAndServe()
 }
