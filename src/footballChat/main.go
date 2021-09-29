9b58f58e58955310122f199093535cd8fb3a5e0f
funeasy <15161451946@163.com>
2021-09-30 07:14:38 +0800

    addcode

--- src/footballChat/main.go
+++ src/footballChat/main.go
@@ -1,7 +1,22 @@
-package main
+package footballchat
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("this is foot ball ")
+	mux := http.NewServeMux()
+
+	//设置路由
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", errors)
+	mux.HandleFunc("/login", login)
+
+	server := http.Server{
+		Addr:    "0.0.0.0:8090",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
